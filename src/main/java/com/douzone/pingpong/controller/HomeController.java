49a87d0e64799e465100f9c816e755dda3bcdbea
hakyeonghoon1 <skyhoon8@gmail.com>
2021-12-01 15:31:35 +0900

    reset

--- src/main/java/com/douzone/pingpong/controller/HomeController.java
+++ src/main/java/com/douzone/pingpong/controller/HomeController.java
@@ -11,28 +11,9 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class HomeController {
     @RequestMapping("/")
     public String home(@SessionAttribute(name = SessionConstants.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
-<<<<<<< HEAD
-		// git TEST...
-        // git TEST2 ...33333  권한READ일때
-        //wonnnnn2222222222
-        //wonnnn
-<<<<<<< HEAD
-=======
-
->>>>>>> ad265011e49f3d529c3e6653035ebfc1cfea7366
-=======
-
-
-        //won222222222
->>>>>>> won
-        return "home";
-        // JIN #1.
-        // JIN #2.
-
-        // pull을 안당기고 수정
-        // branch 안바꾸고 수정
-        // branch 바꾸고 수정
-        // 하에서 메인으로 체크아웃 다시수정
+    
+        return "";
+        //처음부터 다시
     }
 
 }

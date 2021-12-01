c482403942aeb6822e22bb368153d51c06317608
jin0kim326 <jin0kim326@gmail.com>
2021-12-01 12:06:09 +0900

    controller test

--- src/main/java/com/douzone/pingpong/controller/HomeController.java
+++ src/main/java/com/douzone/pingpong/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class HomeController {
     @RequestMapping("/")
     public String home(@SessionAttribute(name = SessionConstants.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
-
+		// git TEST...
         return "home";
     }
 }

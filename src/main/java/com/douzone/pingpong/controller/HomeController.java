c80a690913fc91708ced9f5725359424eb63c7cf
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-12-01 14:25:09 +0900

    init#2

--- src/main/java/com/douzone/pingpong/controller/HomeController.java
+++ src/main/java/com/douzone/pingpong/controller/HomeController.java
@@ -12,6 +12,10 @@ public class HomeController {
     @RequestMapping("/")
     public String home(@SessionAttribute(name = SessionConstants.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
         return "home";
-        // JIN #1
+        // JIN #1.
+        // JIN #2.
     }
+
 }
+
+
